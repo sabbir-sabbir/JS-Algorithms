b80d8520d01e050edefd47392995975a44c1236e
sabbir-sabbir <iosabbir7890@gmail.com>
2024-10-21 00:45:00 +0600

    commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-let data = [10, 20, 30, 40, 50];
-let position = 3;
+const position = document.getElementById("position").value
+function   removeel(){
+    let data = [10, 20, 30, 40, 50];
 
-for (let i = position; i < data.length-1; i++) {
-   data[i]=data[i+1];
-   console.log(data)
+    
+    for (let i = position; i < data.length-1; i++) {
+       data[i]=data[i+1];
+      
+    
+    };
+    data.length= data.length -1 
+    console.log(data)
+    
+    
+}
 
-};
-data.length= data.length -1 
-console.log(data)
 
