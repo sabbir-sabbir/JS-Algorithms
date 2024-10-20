e84b9124d2a67e0177d3f9ffd96fadb0f4e1eadb
sabbir-sabbir <iosabbir7890@gmail.com>
2024-10-21 00:28:09 +0600

    commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let data = [10,20,30,40,50,];
+let data = [10, 20, 30, 40, 50];
 let position = 3;
-for(let i = 0; i <= data.length - 1; i++   );
-console.log(data)
+
+for (let i = 3; i < data.length; i++) {
+   data[i] 
+}
